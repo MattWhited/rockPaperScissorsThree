cc8bc9b5ef1b96aac8c5c694f91ff54cef2f69ae
Mattwhited <matthewwhited@hotmail.com>
2024-09-02 13:03:13 -0500

    Create pseudo code for computer choice function
    I am using pseudo code to work through the logic of
    getting the computers random choice.

--- java.js
+++ java.js
@@ -1,1 +1,11 @@
 console.log('RPS')
+
+// get computer choice is going to be a function
+// that function needs to...
+
+//get random number (between 0-1)
+// multiply that number by 3
+// if output = 1 answer = rock
+// if output = 2 answer = paper
+// if output = 3 answer = scissors
+
