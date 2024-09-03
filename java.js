865741f0c769e2aa1d7ce8b76ee1e86e56911223
Mattwhited <matthewwhited@hotmail.com>
2024-09-03 18:14:37 -0500

    Build out the bulk of the logic
    I need to work on the logic some more.

--- java.js
+++ java.js
@@ -4,11 +4,11 @@ console.log('RPS')
 function getComputerChoice() {
     let Num = Math.floor(Math.random()*3);
     if (Num === 0) {
-        console.log('Rock');
+        console.log('rock');
     } else if (Num === 1) {
-        console.log('Paper');
+        console.log('paper');
     } else if (Num === 2) {
-        console.log('Scissors');
+        console.log('scissors');
     };
 }
 
@@ -23,10 +23,20 @@ let humanScore = 0;
 let computerScore = 0; 
 
 function playRound(humanChoice, computerChoice) {
-    // my code here...
+    if (humanChoice === 'rock' && computerChoice === 'paper') {
+        return 'Computer chose paper, you lost.'
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        return 'Computer chose scissors, better luck next time.'
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
+        return 'Computer chose rock, way to lose again!'
+    } else if (humanChoice === 'rock' && computerChoice === 'rock') {
+        return 'It is a Draw! Play again!'
+    } else {
+        return 'You Win!'
+    }
+    // if the same element is chosen then it is a draw
+    //declare a winner based on those results. 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection)
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice(); 
