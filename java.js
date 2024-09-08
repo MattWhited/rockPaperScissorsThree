ac546ab8456d32101aeaeb1f26a5480bd4dc828d
Mattwhited <matthewwhited@hotmail.com>
2024-09-08 17:01:37 -0500

    Create a function to increment scores
    Super! Flippin! Jazzed!
    
    I figured out how to add points to each player
    just based on the logic I came up with.
    
    Thank You Lord Jesus!

--- java.js
+++ java.js
@@ -13,7 +13,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let answer = prompt("Rock, Paper or Scissors?"); {
+    let answer = prompt ("Rock, Paper or Scissors?"); {
         return answer;
     }
 }
@@ -41,7 +41,46 @@ function playRound() {
         return 'It is a Draw! Play again!';
 }
 
+
+function addPoint() {
+    if (humanChoice === 'rock' && computerChoice === 'paper') {
+        return computerScore++ ;
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+       return  computerScore++ ;
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
+        return computerScore++ ;
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        return humanScore++ ;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        return humanScore++ ; 
+    } else if(humanChoice === 'scissors' && computerChoice === 'paper') {
+        return humanScore++ ;
+    } else if (humanChoice === computerChoice)
+        return 'It is a Draw! Play again!';
+}
+
+
+
 // wether in 'playRound' or just outside of it i need to
 // declare a winner of the round
 // increment the score of the winner of the round by one
 //
+// since I don't understand how ++ works I will
+// play with it down here until i get it. 
+
+let x = 0;
+
+// take an existing value
+// add 1
+// return the new value in place of the old value
+
+// current score = current score + 1
+// because i am adding to that score. 
+
+// function = take the score + 1, return as new score. 
+
+// let x = 0
+// function addPoint(x) {
+// if point is earned 
+// return x++
+// }
